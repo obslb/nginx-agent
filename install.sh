79e2f4dac1b463bbb2daa81f04345272af7adf9d
obslb <obslb@example.com>
2021-08-25 14:35:10 +0300

    install script update

--- install.sh
+++ install.sh
@@ -4,12 +4,8 @@ export LANG=en_US.UTF-8
 sudo apt update && apt install -y git python3.8 python3.8-dev python3.8-venv && apt autoremove --purge -y apache2 && apt dist-upgrade -y
 
 sudo locale-gen en_US.UTF-8
-
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-
-sudo python3.8 -m easy_install pip
-
-
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+#sudo python3.8 -m easy_install pip
 sudo apt install -y nginx letsencrypt redis-server && pipenv install --deploy --system
 
 mkdir -p /etc/nginx-agent/
